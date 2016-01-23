0290bc12400f924fe076bbc61e1fbd8f4142ff8f
David Kroell <dkroell@acm.org>
2016-01-23 17:27:33 -0500

    Removing uneciccary files and making configs more efficient

--- camp_kids_place/src/ckp/src/QuereyDB.java
+++ camp_kids_place/src/ckp/src/QuereyDB.java
@@ -4,8 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public interface QuereyDB {
+public class QuereyDB {
 	public static final String JDBC_URL = "jdbc:derby:ckpdb;create=true";
+	
+	/**
+	 * Executes a sql statement on the Database.
+	 * @param sql
+	 * @return
+	 * @throws SQLException
+	 */
 	public static String sql_statement(String sql) throws SQLException {
 		String result = "";
 		Connection connection = DriverManager.getConnection(JDBC_URL);
